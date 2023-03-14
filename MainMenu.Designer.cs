5a5b23aec6716ec1ce6db984aa0e92d0ff59f14e
MAX TROTTER <M.S.TROTTER-2020@hull.ac.uk>
2023-03-14 19:38:20 +0000

    return to standard

--- MainMenu.Designer.cs
+++ MainMenu.Designer.cs
@@ -141,7 +141,7 @@
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.connectionLabel);
             this.Controls.Add(this.unloadSaveButton);
