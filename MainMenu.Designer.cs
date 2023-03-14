d360ef30cc40d11dc7fa0c74c0e7388292b2818d
MAX TROTTER <M.S.TROTTER-2020@hull.ac.uk>
2023-03-14 19:20:52 +0000

    auto scale mode adjustment

--- MainMenu.Designer.cs
+++ MainMenu.Designer.cs
@@ -141,7 +141,7 @@
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.connectionLabel);
             this.Controls.Add(this.unloadSaveButton);
--- gameBoard.Designer.cs
+++ gameBoard.Designer.cs
@@ -633,7 +633,8 @@ namespace Xiangqi
             // gameBoard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            //this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1102, 791);
             this.Controls.Add(this.myTeamLabel);
             this.Controls.Add(this.saveGameStatusLabel);
